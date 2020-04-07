5890ec16e5931ffe7120260ed283e5e0ef66d16e
amay <1130314149@qq.com>
2020-04-08 06:40:13 +0800

    添加sql配置
    Signed-off-by: amay <1130314149@qq.com>

--- Config/conf.py
+++ Config/conf.py
@@ -9,4 +9,11 @@ def server_ip():
     test_ip='http://qnn-testing-api.maxrupees.com/'
     sit_ip='https://m.maxrupees.com/client/app/business/index.html#/login'
     return dev_ip
+def sql_conf():
+    ip='10.1.0.15'
+    username='root'
+    password=''
+    port=3306
+    charset='utr-8'
+    return ip,username,password,port,charset
 
--- Test_Case/test_fore.py
+++ Test_Case/test_fore.py
@@ -14,8 +14,8 @@ data1={
 	"phone_code":"91"
 }
 url_sms= requests.post(server_ip()+'/api/auth/sms',data=data)
-url_login= requests.post(server_ip()+'/api/login',data=data1)
+#url_login= requests.post(server_ip()+'/api/login',data=data1)
 
 print(url_sms.text)
 print(url_sms.status_code)
-print(url_login.text)
+#print(url_login.text)
