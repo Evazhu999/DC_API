64cedea2cb333783ea6a19ae82589ff99f4cf126
amay <1130314149>
2020-04-08 18:25:43 +0800

    修改返回文案&增加token传参

--- Test_Case/test_fore.py
+++ Test_Case/test_fore.py
@@ -7,14 +7,15 @@
 import requests
 #import time
 from Config.conf import  *
-#headers = {'user-agent': 'my-app/0.0.1'}
 sms_data={"mobile":"917838381868","phone_code":91}
-sms_url= requests.post(server_ip()+'/api/auth/sms',data=sms_data)
-print('验证码发送成功接口返回：',sms_url.text)
+sms_url= requests.post(server_ip()+'/api/auth/sms',json=sms_data)
+print('验证码已发送：',sms_url.text)
 #print(sms_url.status_code)
 #time.sleep(3)
 login_data={"mobile":"917838381868","code":"123456","phone_code":'91'}
-login_url= requests.post(server_ip()+'/api/auth/login',data=login_data)
-print('登录成功接口返回：',login_url.text)
-
+login_url= requests.post(server_ip()+'/api/auth/login',json=login_data)
+print('登录成功：',login_url.text)
+access_token=login_url
+login_token=login_url.json().get(access_token)
+print(login_token)
 
