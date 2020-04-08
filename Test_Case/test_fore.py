00c62c761767171c7dfac52cc7191f05b1904416
amay <1130314149>
2020-04-08 18:07:26 +0800

    test_fore.py文件重写

--- Test_Case/test_fore.py
+++ Test_Case/test_fore.py
@@ -5,18 +5,16 @@
 @File    : test_fore.py
 '''
 import requests
+#import time
 from Config.conf import  *
 #headers = {'user-agent': 'my-app/0.0.1'}
-data={"mobile":"917838381868","phone_code":91}
+sms_data={"mobile":"917838381868","phone_code":91}
+sms_url= requests.post(server_ip()+'/api/auth/sms',data=sms_data)
+print('验证码发送成功接口返回：',sms_url.text)
+#print(sms_url.status_code)
+#time.sleep(3)
+login_data={"mobile":"917838381868","code":"123456","phone_code":'91'}
+login_url= requests.post(server_ip()+'/api/auth/login',data=login_data)
+print('登录成功接口返回：',login_url.text)
 
-url_sms= requests.post(server_ip()+'/api/auth/sms',data=data)
-data1={
-	"mobile":"917838381868",
-	"code":"123456",
-	"phone_code":"91"
-}
-url_login= requests.post(server_ip()+'/api/login',data=data1)
 
-print(url_sms.text)
-print(url_sms.status_code)
-#print(url_login.text)
