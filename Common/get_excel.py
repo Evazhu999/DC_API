93c12a21cdeb4aac9e534893e942f0c09505690c
amay <1130314149>
2020-04-08 16:55:24 +0800

    封装excel数据方法

--- Common/get_excel.py
+++ Common/get_excel.py
@@ -5,13 +5,23 @@ Email:zxm6612719@163.com
 File:get_excel.py
 Describe:'获取excel数据'''
 import xlrd # 导入excel操作的包
-excel=xlrd.open_workbook('../TestData/usermeassage.xlsx')#打开文件，文件的地址要用相对路径
-table=excel.sheets()[0]#读取第一页的信息，0表示第一个，1表示第二页
-print(table.nrows) #取总的行数
-print(table.ncols)#取总的列数
-print(table.rows_values(0))#取第一行的数据
-print(table.col_values(0))#取第一列的数据
-print(table.cell_values(0,1))#取第一行第一列的数据，前面是行后面是列
+def get_excel_row(row):
+ excel=xlrd.open_workbook('../TestData/usermeassage.xlsx')#打开文件，文件的地址要用相对路径
+ table=excel.sheets()[0]#读取第一页的信息，0表示第一个，1表示第二页
+ return table.cell_value(row,1),table.cell_value(row,2)
+'''
+print (table.nrows) #取总的行数
+print (table.ncols)#取总的列数
+print(table.row_values(0))#取第一行的数据
+print (table.col_values(0))#取第一列的数据
+print (table.cell_value(0,1))#取第一行第一列的数据，前面是行后面是列
 for i in range(1,table.nrows):
-    print(table.cell_values(i,1),table.cell_values(i,2))bang
-    'zuibang'
+  print(table.cell_value(i,1),table.cell_value(i,2))
+  '''
+
+
+
+
+
+
+
--- Test_Case/test_fore.py
+++ Test_Case/test_fore.py
@@ -8,13 +8,14 @@ import requests
 from Config.conf import  *
 #headers = {'user-agent': 'my-app/0.0.1'}
 data={"mobile":"917838381868","phone_code":91}
+
+url_sms= requests.post(server_ip()+'/api/auth/sms',data=data)
 data1={
 	"mobile":"917838381868",
 	"code":"123456",
 	"phone_code":"91"
 }
-url_sms= requests.post(server_ip()+'/api/auth/sms',data=data)
-#url_login= requests.post(server_ip()+'/api/login',data=data1)
+url_login= requests.post(server_ip()+'/api/login',data=data1)
 
 print(url_sms.text)
 print(url_sms.status_code)
